43c3f0dcbb87468ec68d5bb255737050d95b5ee3
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-04-07 06:55:04 +0000

    Exemplo

--- src/main/java/com/exemplo/Usuario.java
+++ src/main/java/com/exemplo/Usuario.java
@@ -8,14 +8,13 @@ import java.util.Date; // ou java.time.LocalDate
 
 @Entity
 @NoArgsConstructor @AllArgsConstructor
-@ToString
-@EqualsAndHashCode(of={"id", "email", "idade"})
+@Data
 public class Usuario { 
     @Id @GeneratedValue
-    @Getter private Long id; 
-    @Getter @Setter private String nome;
-    @Getter @Setter private String sobrenome;
-    @Getter @Setter private String email;
-    @Getter @Setter private Date idade;
-    @Getter @Setter private String genero;
+    private Long id; 
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private Date idade;
+    private String genero;
  }
