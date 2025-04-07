3f65a938c35bf0bc6801a6ab7fbbc3fbcb7d2a0a
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-04-07 05:44:23 +0000

    Exemplo

--- src/main/java/com/exemplo/Usuario.java
+++ src/main/java/com/exemplo/Usuario.java
@@ -9,7 +9,7 @@ import java.util.Date; // ou java.time.LocalDate
 @Entity
 @NoArgsConstructor @AllArgsConstructor
 @ToString
-@EqualsAndHashCode
+@EqualsAndHashCode(exclude={"nome", "sobrenome", "genero"})
 public class Usuario { 
     @Id @GeneratedValue
     @Getter private Long id; 
